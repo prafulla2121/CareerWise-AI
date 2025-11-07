0754ba916eab6adabb9a23e2e482dc5382a20009
Prafulla Purohit <prafulpurohit917@gmail.com>
2025-11-07 18:32:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8480343133-c2f30",
+  "appId": "1:359738131259:web:e1184cb567b42eb869ed1e",
+  "apiKey": "AIzaSyDRBY9sLbayhCK8ejH5zHZxmZ_NSTixlaU",
+  "authDomain": "studio-8480343133-c2f30.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "359738131259"
+};
