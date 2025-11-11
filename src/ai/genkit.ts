4d59cb3e6436fd2114092d165da51d706a472d15
Prafulla Purohit <prafulpurohit917@gmail.com>
2025-11-11 17:47:09 +0000

    use 2.5 flash

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,7 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({
+    defaultModel: 'gemini-1.5-flash-latest'
+  })],
 });
